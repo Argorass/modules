4d15f3bde8887049c20a50b02407989e07700bb6
Tomas Matukas <tomas.matukas@gmail.com>
2024-12-12 19:21:45 +0100

    add devide in mathOP

--- mathOperations.js
+++ mathOperations.js
@@ -7,3 +7,11 @@ export function add(num1, num2) {
 export function subtract(num1, num2) {
   return num1 - num2;
 }
+
+//add devide (Return 'Error' if trying to divide by zero)
+export function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Error";
+  }
+  return num1 / num2;
+}
