6c2a62db5b7184f93aeaf59f0e541db02f0471af
Tomas Matukas <tomas.matukas@gmail.com>
2024-12-12 18:40:49 +0100

    creating domElements

---
+++ domElements.js
@@ -0,0 +1,7 @@
+// domElements.js
+
+export const display = document.querySelector("#display");
+export const numberButtons = document.querySelectorAll(".btn.number");
+export const operatorButtons = document.querySelectorAll(".btn.operator");
+export const equalsButton = document.querySelector("#equals");
+export const clearButton = document.querySelector("#clear");
